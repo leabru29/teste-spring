7c4cdf11fa291a1f3903ad77dbe22012eda2e693
Leandro <leandro_ccb10@hotmail.com>
2023-09-01 13:12:39 -0300

    Adicionado repositorio para Perfil

---
+++ src/main/java/com/teste/testespring/repositories/PerfilRepository.java
@@ -0,0 +1,9 @@
+package com.teste.testespring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.teste.testespring.models.Perfil;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+
+}
