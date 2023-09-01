1e73749e17352eb390b8f24a4a884d418afe2d0c
Leandro <leandro_ccb10@hotmail.com>
2023-09-01 15:45:21 -0300

    Validacao para obter perfil por ID

--- src/main/java/com/teste/testespring/services/PerfilServiceImpl.java
+++ src/main/java/com/teste/testespring/services/PerfilServiceImpl.java
@@ -10,14 +10,17 @@ import com.teste.testespring.models.Perfil;
 import com.teste.testespring.repositories.PerfilRepository;
 import com.teste.testespring.services.contracts.PerfilService;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class PerfilServiceImpl implements PerfilService {
 
     @Autowired
     private PerfilRepository perfilRepository;
 
-    @Override
+    @Transactional
     public Perfil createPerfil(Perfil perfil) {
+        perfil.setId(null);
         return perfilRepository.save(perfil);
     }
 
@@ -29,12 +32,13 @@ public class PerfilServiceImpl implements PerfilService {
     @Override
     public Perfil findByIdPerfil(Long id) {
         Optional<Perfil> perfil = perfilRepository.findById(id);
-        return perfil.get();
+        return perfil.orElseThrow(() -> new RuntimeException("Perfil não encontrado."));
     }
 
-    @Override
+    @Transactional
     public Perfil updatePerfil(Perfil perfil) {
-        Perfil existePerfil = perfilRepository.findById(perfil.getId()).get();
+        Perfil existePerfil = perfilRepository.findById(perfil.getId())
+                .orElseThrow(() -> new RuntimeException("Perfil não encontrado."));
         existePerfil.setNome_perfil(perfil.getNome_perfil());
         existePerfil.setAtivo(perfil.getAtivo());
         Perfil updatePerfil = perfilRepository.save(existePerfil);
