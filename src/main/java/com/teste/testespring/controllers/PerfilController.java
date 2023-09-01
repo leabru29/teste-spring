68db628fc98132d7b47d25cc0cd170ce6ea6cf81
Leandro <leandro_ccb10@hotmail.com>
2023-09-01 13:15:07 -0300

    Adicionado a camada de Controller para CRUD do Perfil

---
+++ src/main/java/com/teste/testespring/controllers/PerfilController.java
@@ -0,0 +1,57 @@
+package com.teste.testespring.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teste.testespring.models.Perfil;
+import com.teste.testespring.services.PerfilServiceImpl;
+
+@RestController
+@RequestMapping(value = "/perfil")
+public class PerfilController {
+
+    @Autowired
+    private PerfilServiceImpl perfilServiceImpl;
+
+    @PostMapping
+    public ResponseEntity<Perfil> createPerfil(@RequestBody Perfil perfil) {
+        Perfil novoPerfil = perfilServiceImpl.createPerfil(perfil);
+        return new ResponseEntity<>(novoPerfil, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Perfil>> listPerfil() {
+        List<Perfil> perfil = perfilServiceImpl.findAllPerfil();
+        return new ResponseEntity<>(perfil, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Perfil> getPerfil(@PathVariable Long id) {
+        Perfil perfil = perfilServiceImpl.findByIdPerfil(id);
+        return new ResponseEntity<>(perfil, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Perfil> updatePerfil(@PathVariable Long id, @RequestBody Perfil perfil) {
+        perfil.setId(id);
+        Perfil perfilAtualizado = perfilServiceImpl.updatePerfil(perfil);
+        return new ResponseEntity<>(perfilAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public String deletePerfil(@PathVariable Long id) {
+        perfilServiceImpl.deletePerfil(id);
+        return "Perfil deletado com sucesso!";
+    }
+}
