d553b8b2f75c5ce9f36a7c4dd672693d41cdf8f4
Leandro <leandro_ccb10@hotmail.com>
2023-09-01 14:06:48 -0300

    Adicionado validação no banco de dados e no modelo Perfil com mensagens de erro

--- src/main/java/com/teste/testespring/models/Perfil.java
+++ src/main/java/com/teste/testespring/models/Perfil.java
@@ -1,9 +1,12 @@
 package com.teste.testespring.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,7 +25,12 @@ public class Perfil {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 30, nullable = false, unique = true)
+    @NotBlank(message = "O campo nome precisa ser preenchido.")
+    @Size(min = 3, max = 30, message = "O campo precisa ser preenchido com no mínimo 3 caracteres e no máximo 30 caracteres.")
     private String nome_perfil;
 
+    @Column(nullable = false)
+    @NotBlank(message = "O campo precisa ser preenchido.")
     private Boolean ativo;
 }
