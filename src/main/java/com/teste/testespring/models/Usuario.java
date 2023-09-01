1aa8af24ec09ac63a5dff10a250edb29edfbee4f
Leandro <leandro_ccb10@hotmail.com>
2023-09-01 14:02:11 -0300

    Adicionado validação no banco de dados e no modelo Usuario com mensagens de erro

--- src/main/java/com/teste/testespring/models/Usuario.java
+++ src/main/java/com/teste/testespring/models/Usuario.java
@@ -2,10 +2,13 @@ package com.teste.testespring.models;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -24,11 +27,22 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @Column(name = "nome", length = 60, nullable = false)
+    @NotBlank(message = "Por favor preencha o campo Nome")
+    @Size(min = 3, max = 60, message = "O campo Nome precisa de no mínimo 3 caracteres e no máximo 60 caracteres.")
     private String nome;
 
+    @Column(name = "sobrenome", length = 100, nullable = false)
+    @NotBlank(message = "Por favor preencha o campo Sobrenome")
+    @Size(min = 3, max = 100, message = "O campo Sobrenome precisa de no mínimo 3 caracteres e no máximo 100 caracteres.")
     private String sobrenome;
 
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    @NotBlank(message = "Por favor preencha o campo E-mail")
+    @Size(min = 3, max = 100, message = "O campo E-mail precisa de no mínimo 3 caracteres e no máximo 60 caracteres.")
     private String email;
 
+    @Column(name = "passwd", nullable = false)
+    @NotBlank(message = "Por favor preencha o campo Senha")
     private String senha;
 }
