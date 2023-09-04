35d87de2e775926321703245d97d86849857c0f0
Leandro <leandro_ccb10@hotmail.com>
2023-09-04 07:28:44 -0300

    Configurando excecao para campos invalidos e para Perfil nao encontrado

--- src/main/java/com/teste/testespring/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/teste/testespring/controllers/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,4 +26,21 @@ public class ResourceExceptionHandler {
         error.setPath(request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+
+        FieldError fieldsError = e.getBindingResult().getFieldErrors().get(0);
+        String msgError = "Error " + fieldsError.getDefaultMessage();
+
+        StandardError error = new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.CONFLICT.value());
+        error.setError("Erro em alguns campos para preencher.");
+        error.setMessage(msgError);
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
 }
